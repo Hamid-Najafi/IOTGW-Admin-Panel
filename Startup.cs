cb370d1d72934a88c7659a97b1bba21f7609a2d8
Hamid Najafi <Goldenstarc@gmail.com>
2019-07-03 23:36:44 +0430

    swagger added

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace IOTGW_Admin_Panel
 {
@@ -27,6 +28,28 @@ namespace IOTGW_Admin_Panel
             {
                 configuration.RootPath = "ClientApp/build";
             });
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v0.1",
+                    Title = "IOT Gateway API",
+                    Description = "IOT Gateway API for node and gateway managment",
+                    //TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Hamid Najafi",
+                        Email = "Hamid.Najafi@email.com",
+                        //Url = new Uri("https://twitter.com/spboyer"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        //Url = new Uri("https://example.com/license"),
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,6 +69,16 @@ namespace IOTGW_Admin_Panel
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "IOT Gateway API V0.1");
+            });
 
             app.UseMvc(routes =>
             {

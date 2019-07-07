1c23d1ca5492bb973fca66826147b184f80165a5
Hamid Najafi <Goldenstarc@gmail.com>
2019-07-07 21:06:30 +0430

    fix

--- Startup.cs
+++ Startup.cs
@@ -144,15 +144,15 @@ namespace IOTGW_Admin_Panel
                     template: "{controller}/{action=Index}/{id?}");
             });
 
-            // app.UseSpa(spa =>
-            // {
-            //     spa.Options.SourcePath = "ClientApp";
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
 
-            //         if (env.IsDevelopment())
-            //         {
-            //             spa.UseReactDevelopmentServer(npmScript: "start");
-            //         }
-            //     });
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
+            });
         }
     }
 }
